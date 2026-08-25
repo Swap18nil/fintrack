cfce784b511f9e0b866be6c8ca72d05e86f35ef7
swapnil18 <swapnilsanap97@gmail.com>
2026-08-25 13:51:29 +0100

    Updated sidebar to collapsable

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
 
     return (
         <>
-            <header className="h-16 bg-white border-b border-gray-200 flex items-center px-8">
+            <header className="h-16 bg-white border-b-2 border-gray-200 flex items-center px-8">
                 <h1 className="text-xl font-semibold text-gray-800 capitalize">
                     {pageTitle}
                 </h1>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,11 +2,14 @@ import { Link, useLocation, useNavigate } from "react-router-dom"
 import { useAuthStore } from "../../store/useAuthStore"
 import { LayoutDashboard, ArrowRightLeft, BellRing, LogOut, Wallet } from "lucide-react"
 import { supabase } from "../../core/supabaseClient"
+import { useState } from "react"
 
 export const Sidebar = () => {
     const user = useAuthStore(store => store.user)
     const navigate = useNavigate()
     const location = useLocation()
+
+    const [isSidebarOpen, setIsSideBarOpen] = useState<boolean>(false)
 
     const navItems = [
         { name: 'Overview', path: '/dashboard', icon: LayoutDashboard },
@@ -14,9 +17,14 @@ export const Sidebar = () => {
         { name: 'Reminders', path: '/reminders', icon: BellRing },
     ];
 
-    const handleLogout = async ()=>{
+    const handleLogout = async () => {
         await supabase.auth.signOut();
         navigate('/login')
+
+    }
+
+    const handleCollapse = () => {
+        setIsSideBarOpen(!isSidebarOpen)
 
     }
 
@@ -25,10 +33,14 @@ export const Sidebar = () => {
 
     return (
         <>
-            <aside className="w-64 shrink-0 bg-white border-r border-gray-200 flex flex-col">
-                <div className="h-16 flex items-center px-6 border-b border-gray-200">
+            <aside className={`shrink-0 bg-white border-r-2 border-gray-200 flex flex-col transition-all duration-300 ${isSidebarOpen ? 'w-64' : 'w-22'}`}>
+                <div className="h-16 flex items-center px-6 "
+                    onClick={() => handleCollapse()}>
                     <Wallet className="w-6 h-6 text-blue-600 mr-2" />
-                    <span className="text-xl font-bold text-gray-800">FinTrack</span>
+                    {
+                        (isSidebarOpen && <span className="text-xl font-bold text-gray-800">FinTrack</span>)
+                    }
+
                 </div>
 
                 <nav className="flex-1 px-4 py-6 space-y-1">
@@ -40,34 +52,40 @@ export const Sidebar = () => {
                                 key={item.name}
                                 to={item.path}
                                 className={`flex items-center px-3 py-2.5 text-sm font-medium rounded-lg transition-colors ${isActive
-                                        ? 'bg-blue-50 text-blue-700'
-                                        : 'text-gray-700 hover:bg-gray-100'
+                                    ? 'bg-blue-50 text-blue-700'
+                                    : 'text-gray-700 hover:bg-gray-100'
                                     }`}
                             >
                                 <Icon className={`w-5 h-5 mr-3 ${isActive ? 'text-blue-700' : 'text-gray-400'}`} />
-                                {item.name}
+                                {isSidebarOpen && item.name}
                             </Link>
                         );
                     })}
                 </nav>
 
-                <div className="p-4 border-t border-gray-200">
+                <div className="p-4 border-t-2 border-gray-200">
                     <div className="flex items-center justify-between">
                         <div className="flex items-center space-x-2 truncate">
                             <div className="w-8 h-8 rounded-full bg-blue-100 flex items-center justify-center text-blue-700 font-bold">
                                 {user?.email?.charAt(0).toUpperCase() || 'U'}
                             </div>
-                            <span className="text-sm font-medium text-gray-700 truncate max-w-[120px]">
-                                {user?.email}
-                            </span>
+                            {
+                                isSidebarOpen && <span className="text-sm font-medium text-gray-700 truncate max-w-[120px]">
+                                    {user?.email}
+                                </span>
+                            }
+
                         </div>
-                        <button
-                            onClick={handleLogout}
-                            className="p-2 text-gray-400 hover:text-red-600 rounded-md hover:bg-red-50 transition-colors"
-                            title="Logout"
-                        >
-                            <LogOut className="w-5 h-5" />
-                        </button>
+                        {
+                            isSidebarOpen && <button
+                                onClick={handleLogout}
+                                className="p-2 text-gray-400 hover:text-red-600 rounded-md hover:bg-red-50 transition-colors"
+                                title="Logout"
+                            >
+                                <LogOut className="w-5 h-5" />
+                            </button>
+                        }
+
                     </div>
                 </div>
             </aside>
